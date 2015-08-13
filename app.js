7a2871bb743f5591fa6bc60b4ddfb8f1497b1e87
Eric Ramat <eric.ramat@gmail.com>
2015-08-13 19:08:23 +0200

    Remove user management

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var monk = require('monk');
 var db = monk('localhost:27017/nodetest2');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -33,7 +32,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
