b9004532af8146e4815e8bda4835414a98e56d4a
Eric Ramat <eric.ramat@gmail.com>
2015-08-16 15:33:59 +0200

    Add authentification system

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/primschool');
+// Authentification
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
+var bCrypt = require('bcrypt');
 
 var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -21,20 +28,130 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+// passport configuration
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    var collection = db.get('users');
+
+    collection.find({'_id': id}, {}, function (err, users) {
+        done(err, users[0]);
+    });
+});
+
+var isValidPassword = function (user, password) {
+    return bCrypt.compareSync(password, user.password);
+};
+
+passport.use('login',
+    new localStrategy({
+            passReqToCallback: true
+        },
+        function (req, username, password, done) {
+            // check in mongo if a user with username exists or not
+            var collection = db.get('users');
+
+            collection.find({'username': username}, {},
+                function (err, users) {
+                    // In case of any error, return using the done method
+                    if (err)
+                        return done(err);
+                    // Username does not exist, log error & redirect back
+                    if (users.length == 0) {
+                        console.log('User Not Found with username ' + username);
+                        return done(null, false,
+                            req.flash('message', 'User Not found.'));
+                    }
+                    // User exists but wrong password, log the error
+                    if (!isValidPassword(users[0], password)) {
+                        console.log('Invalid Password');
+                        return done(null, false,
+                            req.flash('message', 'Invalid Password'));
+                    }
+                    // User and password both match, return user from
+                    // done method which will be treated like success
+                    return done(null, users[0]);
+                }
+            );
+        }
+    ));
+
+var createHash = function (password) {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+};
+
+passport.use('signup',
+    new localStrategy({
+            passReqToCallback: true
+        },
+        function (req, username, password, done) {
+            findOrCreateUser = function () {
+                var collection = db.get('users');
+
+                collection.find({'username': username}, {}, function (err, users) {
+                    if (err) {
+                        console.log('Error in SignUp: ' + err);
+                        return done(err);
+                    }
+                    if (users.length > 0) {
+                        console.log('User already exists');
+                        return done(null, false,
+                            req.flash('message', 'User Already Exists'));
+                    } else {
+                        var collection = db.get('users');
+                        var newUser = {
+                            username: username,
+                            password: createHash(password),
+                            email: req.param('email'),
+                            firstName: req.param('firstName'),
+                            lastName: req.param('lastName')
+                        };
+
+                        collection.insert(newUser, function (err, result) {
+                            if (err) {
+                                console.log('Error in Saving user: ' + err);
+                                throw err;
+                            }
+                            console.log('User Registration succesful');
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            };
+            process.nextTick(findOrCreateUser);
+        })
+);
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     req.db = db;
     next();
 });
 
 app.use('/', routes);
+app.use('/users', users);
+app.use('/admin', function (req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+        res.redirect('/');
+    }, function (req, res) {
+        res.render('admin', {user: req.user});
+    }
+);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -45,7 +162,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -56,7 +173,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { });
+  res.render('index', { user: req.user });
 });
+
+router.get('/login', function(req, res) {
+  res.render('login', { message: req.flash('message') });
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/admin',
+  failureRedirect: '/',
+  failureFlash : true
+}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/signup', function(req, res){
+  res.render('signup',{message: req.flash('message')});
+});
+
+router.post('/signup', passport.authenticate('signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash : true
+}));
 
 module.exports = router;
