3a13254e2b598684c42d29f2af68c3744d968739
Eric Ramat <eric.ramat@gmail.com>
2015-08-22 16:15:14 +0200

    Add copyright

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,24 @@
+/**
+ *  This file is part of PrimSchool project.
+ *
+ *  PrimSchool is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This Web application is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PrimSchool.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright     Copyright (c) PrimSchool
+ * @link          http://primschool.org
+ * @license       http://www.gnu.org/licenses/ GPLv3 License
+ */
+
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
